a3951a23572b3fd7bcf9b09cf889171faa5daceb
Kellyzhy <46937935+Kellyzhy@users.noreply.github.com>
2020-02-05 09:47:11 -0500

    C# Inheritance-Customer

---
+++ CustomerAssignment/Customer.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerAssignment
+{
+    class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+
+
+
+        public virtual void AddCustomer()
+        {
+            Console.Write("Enter Id -> ");
+            Id = Convert.ToInt32(Console.ReadLine()); 
+
+            Console.Write("Enter Name -> ");
+            Name = Console.ReadLine();
+
+            Console.Write("Enter Mobile -> ");
+            Email = Console.ReadLine();
+
+            Console.Write("Enter Email -> ");
+            Email = Console.ReadLine();
+        }
+        public virtual void PrintCustomer()
+        {
+            Console.WriteLine("Id = " + Id);
+            Console.WriteLine("Name = " + Name);
+            Console.WriteLine("Mobile = " + Mobile);
+            Console.WriteLine("Email = " + Email);
+        }
+    }
+
+
+    class BronzeCustomer: Customer
+    {
+
+        public string Discount { get; set; }
+        
+        public override void AddCustomer()
+        {
+            base.AddCustomer();
+            Console.Write("Enter discount -> ");
+            Discount = Console.ReadLine();
+        }
+        public override void PrintCustomer()
+        {
+            base.PrintCustomer();
+            Console.WriteLine("Discount = " + Discount);
+            Console.WriteLine("Bronze customer has birthday gift with 1 free samples");
+
+
+        }
+
+    }
+    class SilverCustomer : Customer
+    {
+
+        public string Discount { get; set; }
+        
+        public override void AddCustomer()
+        {
+            base.AddCustomer();
+            Console.Write("Enter discount -> ");
+            Discount = Console.ReadLine();
+        }
+        public override void PrintCustomer()
+        {
+            base.PrintCustomer();
+            Console.WriteLine("Discount = " + Discount);
+            Console.WriteLine("Silver customer has birthday gift with 2 free samples");
+
+        }
+    }
+
+    class GoldCustomer : Customer
+    {
+        public string Discount { get; set; }
+        
+        public override void AddCustomer()
+        {
+            base.AddCustomer();
+            Console.Write("Enter discount -> ");
+            Discount = Console.ReadLine();
+        }
+        public override void PrintCustomer()
+        {
+            base.PrintCustomer();
+            Console.WriteLine("Discount = " + Discount);
+            Console.WriteLine("Gold customer has birthday gift with 3 free samples");
+        }
+    }
+    class PlatinumCustomer : Customer
+    {
+        public string Discount { get; set; }
+        
+        public override void AddCustomer()
+        {
+            base.AddCustomer();
+            Console.Write("Enter discount -> ");
+            Discount = Console.ReadLine();
+        }
+        public override void PrintCustomer()
+        {
+            base.PrintCustomer();
+            Console.WriteLine("Discount = " + Discount);
+            Console.WriteLine("Platinum customer has birthday gift with 4 free samples");
+        }
+    }
+}
+
---
+++ CustomerAssignment/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //BronzeCustomer bronzeCustomer = new BronzeCustomer();
+            //bronzeCustomer.AddBronzeCustomer();
+            //bronzeCustomer.PrintBronzeCustomer();
+
+            Console.WriteLine("Press 1 to Add Bronze customer");
+            Console.WriteLine("Press 2 to Add Silver customer");
+            Console.WriteLine("Press 3 to Add Gold customer");
+            Console.WriteLine("Press 4 to Add Platinum customer");
+            Console.WriteLine("Enter your Option -> ");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+            Customer customer = new Customer();
+            switch (choice)
+            {
+                case 1:
+                    customer = new BronzeCustomer();
+                    break;
+                case 2:
+                    customer = new SilverCustomer();
+                    break;
+                case 3:
+                    customer = new GoldCustomer();
+                    break;
+                case 4:
+                    customer = new PlatinumCustomer();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Option");
+                    break;
+            }
+            if (customer != null)
+            {
+                customer.AddCustomer();
+                customer.PrintCustomer();
+            }
+            Console.Read();
+        }
+    }
+            
+}
